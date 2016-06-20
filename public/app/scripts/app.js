ce5ad85fd39e7617d04764809cc029ef8517bde6
XamNiner <praxis@praxiss-Mac-Pro.local>
2016-06-20 11:09:32 +0200

    change app for layout

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -14,24 +14,22 @@ angular
     'ngRoute'
   ])
     
-  .config(function ($routeProvider) {
-    $routeProvider
-    //existing room
-      .when('/room/:roomId', {
-        templateUrl: 'views/room.html',
-        controller: 'RoomCtrl'
-      })
-    //standard room for new entrants 
-      .when('/room', {
-        templateUrl: 'views/room.html',
-        controller: 'RoomCtrl',
-      })
-      .otherwise({
-        redirectTo: '/room'
-      });
-  })
-
-
+    .config(function ($routeProvider) {
+        $routeProvider
+        //existing room
+        .when('/room/:roomId', {
+            templateUrl: 'views/room.html',
+            controller: 'RoomCtrl'
+        })
+        //standard room for new entrants 
+        .when('/room', {
+            templateUrl: 'views/room.html',
+            controller: 'RoomCtrl',
+        })
+        .otherwise({
+            redirectTo: '/room'
+        });
+    })
     .constant('config', {
         //url for the application to connect server with socket.io
         SIGNALING_SERVER_URL: 'https://rocky-citadel-89998.herokuapp.com' 
